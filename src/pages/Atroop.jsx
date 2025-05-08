300719df52b331d63137a613e2ea7e7b188ca5bd
nono20794 <chardon@post.bgu.ac.il>
2025-05-08 16:49:30 +0300

    add input fields

--- src/pages/Atroop.jsx
+++ src/pages/Atroop.jsx
@@ -1,23 +1,95 @@
 import Tabs from "../components/Tabs";
+import "./Atroop.css";
 
 export default function Atroop() {
   return (
     <section className="troop-container atroop-container">
       <h1>סוללה א</h1>
       <Tabs>
-        <div className="section-1" title="שמירות">
-          <div className="commandor-name">
-            <label htmlFor="name" className="label">
-              שם מפקד:
+        <div className="section-1" title="סיורים">
+          <div className="input-container">
+            <label htmlFor="force" className="label">
+              כוח:
             </label>
-            <input type="text" name="name" id="name" placeholder="שם מפקד" />
+            <select name="force" id="force" className="input-field">
+              <option value="" disabled>
+                -- Choose a color --
+              </option>
+              <option value="red">Red</option>
+              <option value="orange">Orange</option>
+              <option value="yellow">Yellow</option>
+              <option value="green">Green</option>
+              <option value="blue">Blue</option>
+              <option value="indigo">Indigo</option>
+              <option value="violet">Violet</option>
+            </select>
+          </div>
+          <div className="input-container">
+            <label htmlFor="location" className="label">
+              מיקום:
+            </label>
+            <input
+              type="text"
+              name="location"
+              id="location"
+              placeholder="מיקום"
+              className="input-field"
+            />
+          </div>
+          <div className="input-container">
+            <label htmlFor="nightvision" className="label">
+              צ' אמרל:
+            </label>
+            <select name="nightvision" id="nightvision" className="input-field">
+              <option value="" disabled>
+                -- Choose a color --
+              </option>
+              <option value="red">Red</option>
+              <option value="orange">Orange</option>
+              <option value="yellow">Yellow</option>
+              <option value="green">Green</option>
+              <option value="blue">Blue</option>
+              <option value="indigo">Indigo</option>
+              <option value="violet">Violet</option>
+            </select>
+          </div>
+          <div className="input-container">
+            <label htmlFor="binoculars" className="label">
+              צ' משקפת:
+            </label>
+            <select name="binoculars" id="binoculars" className="input-field">
+              <option value="" disabled>
+                -- Choose a color --
+              </option>
+              <option value="red">Red</option>
+              <option value="orange">Orange</option>
+              <option value="yellow">Yellow</option>
+              <option value="green">Green</option>
+              <option value="blue">Blue</option>
+              <option value="indigo">Indigo</option>
+              <option value="violet">Violet</option>
+            </select>
+          </div>
+          <div className="input-container">
+            <label htmlFor="launcher" className="label">
+              צ' מטול:
+            </label>
+            <select name="launcher" id="launcher" className="input-field">
+              <option value="" disabled>
+                -- Choose a color --
+              </option>
+              <option value="red">Red</option>
+              <option value="orange">Orange</option>
+              <option value="yellow">Yellow</option>
+              <option value="green">Green</option>
+              <option value="blue">Blue</option>
+              <option value="indigo">Indigo</option>
+              <option value="violet">Violet</option>
+            </select>
           </div>
         </div>
         <div className="section-2" title="עמדות">
-          <h1>this is section 2</h1>
-        </div>
-        <div className="section-3" title="סיורים">
-          <h1>this is section 3</h1>
+          <h2>this is section 2</h2>
         </div>
       </Tabs>
     </section>
